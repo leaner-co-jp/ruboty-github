d7edd0d8c1e2fbec468fed92c1d8dcb80fa83c55
kokuyouwind <kokuyouwind@gmail.com>
2021-09-27 11:15:17 +0900

    show undeployed で複数リポジトリに対応

--- lib/ruboty/github/actions/show_undeployed_pull_requests.rb
+++ lib/ruboty/github/actions/show_undeployed_pull_requests.rb
@@ -9,7 +9,9 @@ module Ruboty
           when !has_access_token?
             require_access_token
           else
-            show
+            repos.each do |repo|
+              show(repo)
+            end
           end
         rescue Octokit::Unauthorized
           message.reply("Failed in authentication (401)")
@@ -22,36 +24,38 @@ module Ruboty
 
         private
 
-        def show
-          if merge_pull_requests.empty?
-            message.reply 'No pull requests have been merged since the last deployment.'
+        def show(repo)
+          pull_requests = merge_pull_requests(repo)
+          if pull_requests.empty?
+            message.reply "#{repo} には未リリースのPRはないようだぞ！"
           else
+            message.reply "#{repo} には以下の未リリースPRがあるぞ！"
             r = Regexp.union(
               /\AMerge pull request (?<number>\#\d+).*\n\n(?<title>.+)/,
               /\A(?<title>.+) \((?<number>#\d+)\)/)
-            merge_pull_requests.each do |text|
+            pull_requests.each do |text|
               m = text.match(r) { |t| "#{t[:number]} #{t[:title]}" }
               message.reply m
             end
           end
         end
 
-        def merge_pull_requests
-          @merge_pull_requests ||= commits[:commits].map {|commit|
+        def merge_pull_requests(repo)
+          commits(repo)[:commits].map {|commit|
             commit[:commit][:message]
           }.grep(/(\A.+ \(#\d+\)|\AMerge pull request)/)
         end
 
-        def commits
-          @commits ||= client.compare(repo, latest_deploy, 'master')
+        def commits(repo)
+          client.compare(repo, latest_deploy, 'develop')
         end
 
         def latest_deploy
-          'release/main'
+          'release'
         end
 
-        def repo
-          message[:repo]
+        def repos
+          message[:repo].split(',')
         end
       end
     end
