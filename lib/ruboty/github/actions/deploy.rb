ecb948115023acd30d924887a8591a6a6fbbb5be
kokuyouwind <kokuyouwind@gmail.com>
2021-10-20 11:17:41 +0900

    fix undefined variable error

--- lib/ruboty/github/actions/deploy.rb
+++ lib/ruboty/github/actions/deploy.rb
@@ -48,7 +48,7 @@ module Ruboty
           r2 = /\AMerge pull request (?<number>\#\d+) from .*\/dependabot\/(?<title>.+)\z/
           pull_requests.map do |text|
             text.match(r) { |t| "#{t[:number]} #{t[:title]}" } ||
-              text.match(r2) { |t| "#{t[:number]} Bump #{t[:title]} https://github.com/#{repo}/pull/#{t[:number].gsub('#', '')}" }
+              text.match(r2) { |t| "#{t[:number]} Bump #{t[:title]}" }
           end.join("\n")
         end
 
