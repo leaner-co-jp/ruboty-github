4a477b8bbf75ca3ef59a4228bc503e9dee6912dc
kokuyouwind <kokuyouwind@gmail.com>
2021-10-20 09:56:28 +0900

    DeployにもDependabot対応を反映

--- lib/ruboty/github/actions/deploy.rb
+++ lib/ruboty/github/actions/deploy.rb
@@ -45,8 +45,10 @@ module Ruboty
           r = Regexp.union(
             /\AMerge pull request (?<number>\#\d+).*\n\n(?<title>.+)/,
             /\A(?<title>.+) \((?<number>#\d+)\)/)
+          r2 = /\AMerge pull request (?<number>\#\d+) from .*\/dependabot\/(?<title>.+)\z/
           pull_requests.map do |text|
-            text.match(r) { |t| "#{t[:number]} #{t[:title]}" }
+            text.match(r) { |t| "#{t[:number]} #{t[:title]}" } ||
+              text.match(r2) { |t| "#{t[:number]} Bump #{t[:title]} https://github.com/#{repo}/pull/#{t[:number].gsub('#', '')}" }
           end.join("\n")
         end
 
